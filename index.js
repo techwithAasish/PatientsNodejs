4238afe68b250c2b6e498f8323a7f9c39641df79
techwithAasish <aasish.mahato101@gmail.com>
2023-11-30 19:16:11 -0500

    host added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let SERVER_NAME = "patient-api";
-let PORT = 10000;
+let PORT = 3000;
 // let HOST = "192.168.2.95";
-let HOST = "127.0.0.1";
+// let HOST = "127.0.0.1";
+let HOST = "0.0.0.0";
 // let HOST = "10.24.32.80";
 
 const { log } = require("console");
